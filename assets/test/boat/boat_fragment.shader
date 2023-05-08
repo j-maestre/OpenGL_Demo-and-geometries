675a6172b159fbd4de92545112222074f59d427f
j-maestre <xemaiestacio@gmail.com>
2023-05-08 11:14:04 +0200

    adding boat obj

---
+++ assets/test/boat/boat_fragment.shader
@@ -0,0 +1,154 @@
+#version 330
+
+layout(location = 0) out vec4 fragColor;
+
+struct DirLight
+{
+  float active;
+  vec3 dir;
+  vec3 diffuse_color;
+  vec3 specular_color;
+  float specular_strength;
+  float specular_shininess;
+};
+
+struct PointLight
+{
+  float active;
+  vec3 pos;
+  vec3 diffuse_color;
+  vec3 specular_color;
+  float specular_strength;
+  float specular_shininess;
+  float constant_att;
+  float linear_att;
+  float quadratic_att;
+};
+
+struct SpotLight
+{
+  float active;
+  vec3 pos;
+  vec3 dir;
+  float cutt_off;
+  float outer_cut_off;
+  vec3 diffuse_color;
+  vec3 specular_color;
+  float specular_strength;
+  float specular_shininess;
+  float constant_att;
+  float linear_att;
+  float quadratic_att;
+};
+
+uniform sampler2D u_diffuse_color;
+uniform sampler2D u_specular_color;
+uniform vec3 u_camera_position;
+uniform float u_time;
+uniform DirLight u_dirLight;
+uniform vec4 u_color;
+//uniform PointLight u_pointLight[4];
+//uniform SpotLight u_spotLight[4];
+
+in vec2 s_uv;
+in vec3 s_normal;
+in vec3 s_fragPos;
+
+//vec3 direction = vec3(0.0, 1.0, 0.0);
+//vec3 diffuse = vec3(1.0, 0.5, 0.0);
+
+vec3 CalculeDirLight(DirLight light, vec3 normal, vec3 viewDir) {
+
+  /*---Difuse---*/
+  float diff = max(dot(normal, light.dir), 0.0);
+  vec3 diffuse = diff * light.diffuse_color * u_color.xyz;
+
+  /*---Specular---*/
+
+  vec3 reflectDir = normalize(reflect(-(light.dir), normalize(normal))  );
+  float spec = pow(max(dot(normalize(viewDir), normalize(reflectDir)), 0.0), light.specular_shininess);
+  vec3 specular = light.specular_strength * spec * light.specular_color * u_color.xyz;
+
+  return (diffuse + specular);
+}
+
+vec3 CaluclePointLight(PointLight light, vec3 normal, vec3 viewDir, vec3 fragPos)
+{
+  vec3 lightDir = normalize(light.pos - fragPos);
+
+  /*---Diffuse---*/
+  float diff = max(dot(normal, lightDir), 0.0);
+  vec3 diffuse = light.diffuse_color * diff * u_color.xyz;
+
+  /*---Specular---*/
+  vec3 reflectDir = reflect(-lightDir, normal);
+  float spec = pow(max(dot(viewDir, reflectDir), 0.0), light.specular_shininess);
+  vec3 specular = (light.specular_strength * spec * light.specular_color) * u_color.xyz;
+
+  /*---Attenuation---*/
+  float distance = length(light.pos - fragPos);
+  float attenuation = 1.0 / (light.constant_att + light.linear_att * distance + 
+                            light.quadratic_att * (distance * distance));
+
+
+  diffuse *= attenuation;
+  specular *= attenuation;
+
+  return (diffuse + specular);
+}
+
+vec3 CalculeSpotLight(SpotLight light, vec3 normal, vec3 viewDir, vec3 fragPos) 
+{
+  vec3 lightDir = normalize(light.pos - fragPos);
+  float theta = dot(lightDir, normalize(-light.dir));
+
+  vec3 diffuse = vec3(0.0);
+  vec3 specular = vec3(0.0);
+
+  if (theta > light.outer_cut_off)
+  {
+    /*---Diffuse---*/
+    float diff = max(dot(normal, lightDir), 0.0);
+    diffuse = light.diffuse_color * diff * u_color.xyz;
+    /*---Specular---*/
+    vec3 reflectDir = reflect(-lightDir, normal);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), light.specular_shininess);
+    specular = (light.specular_strength * spec * light.specular_color * u_color.xyz);
+    
+    float distance = length(light.pos - fragPos);
+    float attenuation = 1.0 / (light.constant_att + light.linear_att * distance + 
+                              light.quadratic_att * (distance * distance));
+
+    float epsilon = light.cutt_off - light.outer_cut_off;
+    float intensity = clamp((theta - light.outer_cut_off) / epsilon, 0.0, 1.0);
+
+    diffuse *= attenuation;
+    specular *= intensity;
+
+    diffuse *= intensity;
+    specular *= intensity;
+
+  }
+  return (diffuse + specular);
+}
+
+void main() { 
+  vec3 view_direction = normalize(u_camera_position - s_fragPos);
+
+  float ambient_strength = 0.1;
+  vec3 ambient_color = vec3(1.0);
+  vec3 ambient = ambient_strength * ambient_color;
+
+  vec3 color = ambient;
+
+  DirLight light = u_dirLight;
+
+  //light.dir.x *= cos(u_time * 0.0005); 
+  //light.dir.y *= sin(u_time * 0.0005); 
+  
+
+  color += CalculeDirLight(light, s_normal, view_direction);
+
+  //fragColor = vec4(color, 1.0);
+  fragColor = vec4(1.0, 0.0, 0.0, 1.0);
+}
--- tests/main.cc
+++ tests/main.cc
@@ -14,7 +14,9 @@
 
 #include "EDK3/geometry.h"
 #include "EDK3/camera.h"
+#include "camera_custom.h"
 #include "EDK3/drawable.h"
+#include "EDK3/matdiffuse.h"
 #include "EDK3/matdiffusetexture.h"
 #include "EDK3/texture.h"
 #include "Lights.h"
@@ -28,7 +30,7 @@
 
 //Unnamed struct and it's unique instance:
 struct {
-  EDK3::ref_ptr<EDK3::Camera> camera;
+  EDK3::ref_ptr<EDK3::CameraCustom> camera;
   EDK3::ref_ptr<EDK3::Node> root;
 } GameState;
 
@@ -61,9 +63,18 @@ void InitScene() {
   terrain.alloc();
   terrain->init(kTerrainWidth, kTerrainHeight);
 
+  // Create water
   EDK3::ref_ptr<EDK3::TerrainCustom> water;
   water.alloc();
   water->init(kTerrainWidth,kTerrainHeight, 0.2f);
+
+  // Create boat
+  EDK3::scoped_array<EDK3::ref_ptr<EDK3::Geometry>> boat;
+  EDK3::LoadObj("./test/boat/boat.obj", &boat, nullptr);
+  if(!boat.get()){
+    printf("No hay BOAT OBJ!!!\n");
+  }
+
 
 
   // Create material
@@ -75,6 +86,12 @@ void InitScene() {
   mat_water.alloc();
   mat_water->init("./test/water_vertex.shader","./test/water_fragment.shader");
 
+  EDK3::ref_ptr<EDK3::MatDiffuse> mat_boat;
+  mat_boat.alloc();
+  //mat_boat->init("./test/boat/boat_vertex.shader", "./test/boat/boat_fragment.shader");
+
+
+
 
   // Create texture
   EDK3::ref_ptr<EDK3::Texture> t_texture;
@@ -82,6 +99,13 @@ void InitScene() {
 
   EDK3::ref_ptr<EDK3::Texture> w_texture;
   EDK3::Texture::Load("./test/T_EDK_Logo.png", &w_texture);
+
+  EDK3::ref_ptr<EDK3::Texture> b_texture;
+  //EDK3::Texture::Load("./test/boat/boat_img.jpg", &b_texture);
+  EDK3::Texture::Load("./test/T_EDK_Logo.png", &b_texture);
+  if(!b_texture){
+    printf("No hay textura del boat!!!\n");
+  }
 
   // Material custom settings
 
@@ -94,13 +118,20 @@ void InitScene() {
   mat_settings->set_color(terrain_color);
 
   // Water
-
   float water_color[4] = {0.0f, 0.0f, 1.0f, 1.0f};
   EDK3::ref_ptr<EDK3::MaterialCustom::MaterialCustomSettings> water_mat_settings;
   water_mat_settings.alloc();
   water_mat_settings->set_diffuse_texture(w_texture);
   water_mat_settings->set_specular_texture(w_texture);
   water_mat_settings->set_color(water_color);
+
+  // Boat
+  float boat_color[4] = {1.0f, 0.0f, 0.0f, 1.0f};
+  EDK3::ref_ptr<EDK3::MatDiffuseTexture::Settings> mat_sett_boat;
+  mat_sett_boat.alloc();
+  mat_sett_boat->set_color(boat_color);
+
+
 
 
   // Lights
@@ -120,9 +151,12 @@ void InitScene() {
   dirLight->specular_shininess = 1.0f;
   mat_settings->set_dir_light(dirLight);
   water_mat_settings->set_dir_light(dirLight);
+  //mat_sett_boat->set_dir_light(dirLight);
 
   // Drawable
   EDK3::ref_ptr<EDK3::Drawable> node;
+  EDK3::ref_ptr<EDK3::Node> root_2;
+  root_2.alloc();
 
   // Terrain
   node.alloc();
@@ -132,6 +166,24 @@ void InitScene() {
   node->set_position(-1.0f, 0.0f, -1.0f);
   root->addChild(node.get());  
 
+
+  
+  // Boat
+  EDK3::ref_ptr<EDK3::Drawable> drawable;
+  EDK3::ref_ptr<EDK3::Node> boat_node;
+  
+  boat_node.alloc();
+  for(int i = 0; i < boat.size(); i++){
+    drawable.alloc();
+    drawable->set_geometry(boat[i].get());
+    drawable->set_material(mat_boat.get());
+    drawable->set_material_settings(mat_sett_boat.get());
+    drawable->set_position(0.0f, 5.0f, 0.0f);
+    boat_node->addChild(drawable.get());
+  }
+  root->addChild(boat_node.get());
+  
+
   // Water
   node.alloc();
   node->set_geometry(water.get());
@@ -140,30 +192,43 @@ void InitScene() {
   node->set_position(-1.0f,0.0f,-1.0f);
   root->addChild(node.get()); 
 
+
+  //root->addChild(root_2.get());
   //Allocating and initializing the camera:
   GameState.camera.alloc();
-  float pos[] = { 120.0f / 2.0f, 140.0f / 2.0f, 120.0f / 2.0f };
-  float view[] = { -120.0f, -140.0f, -120.0f };
+  //float pos[] = { 120.0f / 2.0f, 140.0f / 2.0f, 120.0f / 2.0f };
+  //float view[] = { -120.0f, -140.0f, -120.0f };
+
+  float pos[] = { 0.0f, 4.0f, 40.0f };
+  float view[] = { 0.0f, 0.0f, -6.0f };
   GameState.camera->set_position(pos);
-  GameState.camera->set_view_direction(view);
+  GameState.camera->set_view_target(view);
+  GameState.camera->setEnabled(true);
+  GameState.camera->setSpeed(0.02f);
+  GameState.camera->setSensitibity(1.0f);
+  GameState.camera->initViewTarget(kWindowWidth,kWindowHeight);
+
   GameState.camera->setupPerspective(70.0f, 8.0f / 6.0f, 1.0f, 1500.0f);
-  
   mat_settings->set_camera_position(GameState.camera->position());
   water_mat_settings->set_camera_position(GameState.camera->position());
   
   EDK3::dev::GPUManager::CheckGLError("Prepare END");
 }
 
-void UpdateFn() {
+void UpdateFn(double dt) {
   GameState.camera->set_clear_color(0.94f, 1.0f, 0.94f, 1.0f);
+  GameState.camera->update(dt, kWindowWidth, kWindowHeight);
+  GameState.camera->set_clear_color(0.94f, 1.0f, 0.94f, 1.0f);
+
+  EDK3::ref_ptr<EDK3::Node> boat = GameState.root->child(1);
+  boat->set_scale(0.5f, 0.5f, 0.5f);
+  boat->set_rotation_xyz(ESAT::Time() * 0.005f, ESAT::Time() * 0.005f, ESAT::Time() * 0.005f);
+  //boat->set_scale(0.2f, 0.2f, 0.2f);
 }
 
 void RenderFn() {
   //For every frame... determine what's visible:
   GameState.camera->doCull(GameState.root.get());
-
-  //dynamic_cast<EDK3::TerrainCustom>(GameState.root->child(0));
-  //Oleaje( GameState.root->child(1));
 
   //Rendering the scene:
   EDK3::dev::GPUManager::CheckGLError("begin Render-->");
@@ -175,6 +240,34 @@ void ImGuiFn(double dt) {
   ImGui::Begin("FPS Window");
   ImGui::Text("FPS: %0.1f", 1000.0 / dt);
   ImGui::Text("Delta time: %0.3f ms", dt);
+  if(ImGui::CollapsingHeader("Camera")){
+    ImGui::Text("View target");
+    ESAT::Vec3 view_dir = GameState.camera->direction(); 
+    const float* view = GameState.camera->target(); 
+    ImGui::Text("X: %f",view[0]);
+    ImGui::Text("Y: %f",view[1]);
+    ImGui::Text("Z: %f",view[2]);
+    
+    ImGui::Text("View direction");
+    ImGui::DragFloat("X: ",&view_dir.x,0.005f,-100.0f,100.0f, "%f");
+    ImGui::DragFloat("Y: ",&view_dir.y,0.005f,-100.0f,100.0f, "%f");
+    ImGui::DragFloat("Z: ",&view_dir.z,0.005f,-100.0f,100.0f, "%f");
+
+    const float* pos = GameState.camera->position();
+    ImGui::Text("Position");
+    ImGui::Text("X: %f",pos[0]);
+    ImGui::Text("Y: %f",pos[1]);
+    ImGui::Text("Z: %f",pos[2]);
+
+    ImGui::Text("Speed");
+    float movementSpeed = GameState.camera->speed();
+    ImGui::DragFloat("Movement", &movementSpeed, 0.01f, 0.0f,2.0f, "%f");
+    GameState.camera->setSpeed(movementSpeed);
+    float sensitivity = GameState.camera->sensitivity();
+    ImGui::DragFloat("Sensitivity", &sensitivity, 0.01f, 0.0f,4.0f, "%f");
+    GameState.camera->setSensitibity(sensitivity);
+
+  }
   ImGui::End();
   ImGui::Render();
 }
@@ -186,7 +279,7 @@ int ESAT::main(int argc, char** argv) {
   double last_time = ESAT::Time();
   while (!ESAT::IsSpecialKeyDown(ESAT::kSpecialKey_Escape) && 
          ESAT::WindowIsOpened()) {
-    UpdateFn();
+    UpdateFn(dt);
     RenderFn();
     dt = ESAT::Time() - last_time;
     last_time = ESAT::Time();
