06197415dda848257bccde1e3886b643f73b06ee
j-maestre <xemaiestacio@gmail.com>
2023-06-01 21:04:18 +0200

    add islands and adjust illumination

--- tests/main.cc
+++ tests/main.cc
@@ -68,8 +68,8 @@ const int kTerrainWidth = 1024;
 const int kTerrainHeight = 1024;
 
 bool followBoat_ = false;
-ESAT::Vec3 boatPos = {0.0f, 0.773f, 0.0f};
-ESAT::Vec3 faroPos = {121.0f, -51.0f, 100.0f};
+ESAT::Vec3 boatPos = {0.0f, 0.773f, -75.0f};
+ESAT::Vec3 faroPos = {162.7f, -51.0f, 3.3f};
 EDK3::ref_ptr<DirLight> dirLight;
 EDK3::ref_ptr<PointLight> pointLight;
 EDK3::ref_ptr<SpotLight> spotLight;
@@ -82,12 +82,23 @@ const float *axes;
 bool rotate_spot_light = true;
 float rotation_speed = 5.0f;
 
+float terrain_pos[3] = {4.2f, 0.0f, -199.7f};
+float terrain_scale[3] = {2.0f, 1.0f, 1.0f};
+
 float sphere_pivot[3];
 float sphere_pos[3] = {0.0f, 0.0f, 0.0f};
 float sphere_rotation_speed = 0.2f;
 
-float surface_position[3] = {0.0f, 15.0f, 0.0f};
+float surface_position[3] = {-152.99f, 48.04f, -203.81f};
 float surface_rotation_speed = 0.2f;
+float surface_scale[3] = {73.0f, 66.1f, 67.6f};
+
+float island_pos[3] = {200.0f, 1.2f, -200.0f};
+float island_rotation = 107.4f;
+float island_scale = 0.9f;
+
+float isla_guapa_pos[3] = {6.7f, 11.6f, 260.2f};
+float isla_guapa_rotation[3] = {0.0f, -0.7f, 0.0f};
 
 
 char controller_type;
@@ -99,6 +110,39 @@ static float gTriangle[] = {
   0.0f, 1.0f, 
   1.0f, 0.0f, 
   0.0f, -1.0f
+};
+
+static float cascada[] = {
+-0.00 , 1.00,
+0.07 , 1.00,
+0.13 , 0.97,
+0.21 , 0.90,
+0.27 , 0.80,
+0.32 , 0.64,
+0.34 , 0.47,
+0.37 , 0.30,
+0.41 , 0.00,
+0.46 , -0.35,
+0.48 , -0.49,
+0.51 , -0.56,
+0.67 , -0.66,
+0.78 , -0.69,
+0.91 , -0.72,
+0.99 , -0.75,
+0.99 , -0.80,
+0.99 , -0.89,
+0.98 , -0.95,
+0.96 , -0.98,
+0.92 , -0.98,
+0.82 , -0.98,
+0.73 , -0.98,
+0.59 , -0.98,
+0.47 , -0.99,
+0.38 , -0.99,
+0.24 , -0.99,
+0.16 , -0.99,
+0.07 , -0.98,
+0.00 , -0.98
 };
 
 static float surface_points[] = {
@@ -167,7 +211,15 @@ static float otherPoints[] = {
     0.00 , -0.34
 };
 
+static float circleSurface[720];
 
+void CreateCircle(int num_revs){
+  float angle = 6.28 / num_revs;
+  for(int i = 0; i < num_revs; i++){
+    circleSurface[i * 2] = cosf(i* angle) + 5.0f;
+    circleSurface[i * 2 + 1] = sinf(i* angle);
+  }
+}
 
 void MoveBoat(double dt){
 
@@ -227,12 +279,12 @@ void InitScene() {
   // Create terrain
   EDK3::ref_ptr<EDK3::TerrainCustom> terrain;
   terrain.alloc();
-  terrain->init(&wireFrame_, kTerrainWidth, kTerrainHeight, 1.2f);
+  terrain->init(kTerrainWidth>>1, kTerrainHeight>>2, 1.2f);
 
   // Create water
   EDK3::ref_ptr<EDK3::TerrainCustom> water;
   water.alloc();
-  water->init(&wireFrame_, kTerrainWidth,kTerrainHeight, 0.2f);
+  water->init(kTerrainWidth,kTerrainHeight, 0.2f);
 
   // Create boat
   EDK3::scoped_array<EDK3::ref_ptr<EDK3::Geometry>> boat;
@@ -249,10 +301,18 @@ void InitScene() {
     printf("No hay FARO OBJ!!!\n");
   }
 
+  // Island
+  EDK3::scoped_array<EDK3::ref_ptr<EDK3::Geometry>> island;
+  EDK3::LoadObj("./test/island/island.obj", &island, nullptr);
+  
+  // Island Guapa
+  EDK3::scoped_array<EDK3::ref_ptr<EDK3::Geometry>> island_guapa;
+  EDK3::LoadObj("./test/island_guapa.obj", &island_guapa, nullptr);
+
   // Create sphere
   EDK3::ref_ptr<EDK3::SphereCustom> sphere;
   sphere.alloc();
-  sphere->init(&wireFrame_, 3.0f);
+  sphere->init(3.0f);
 
   // Create cube map
   EDK3::ref_ptr<CustomGPUCubeMap> cube_map;
@@ -261,13 +321,15 @@ void InitScene() {
 
   EDK3::ref_ptr<EDK3::CubeCustom> cube_geo;
   cube_geo.alloc();
-  cube_geo->init(&wireFrame_,1.0f, false, true);
+  cube_geo->init(1.0f, false, true);
 
   // Create surface
+  CreateCircle(360);
   EDK3::ref_ptr<EDK3::SurfaceCustom> surface;
   surface.alloc();
   //surface->init(surface_points, 30, 32);
-  surface->init(otherPoints,30, 30);
+  //surface->init(otherPoints,30, 30);
+  surface->init(cascada,30, 360, false);
 
 
 
@@ -291,6 +353,14 @@ void InitScene() {
   // Faro
   EDK3::ref_ptr<EDK3::MatDiffuse> mat_faro;
   mat_faro.alloc();
+  
+  // Island
+  EDK3::ref_ptr<EDK3::MatDiffuseTexture> mat_island;
+  mat_island.alloc();
+  
+  // Island Guapa
+  EDK3::ref_ptr<EDK3::MatDiffuseTexture> mat_island_guapa;
+  mat_island_guapa.alloc();
 
   // Sphere
   EDK3::ref_ptr<EDK3::MaterialCustom> mat_sphere;
@@ -329,7 +399,7 @@ void InitScene() {
 
   // Surface
   EDK3::ref_ptr<EDK3::Texture> surface_texture;
-  EDK3::Texture::Load("./test/T_Chopper.jpg", &surface_texture);
+  EDK3::Texture::Load("./test/watter_diff.png", &surface_texture);
   if(!surface_texture){
     printf("No hay textura de la surfaceeeee!!!\n");
   }
@@ -348,6 +418,22 @@ void InitScene() {
   EDK3::Texture::Load("./test/T_EDK_Logo.png", &f_texture);
   if(!f_texture){
     printf("No hay textura del faro!!!\n");
+  }
+  
+  // Island
+  EDK3::ref_ptr<EDK3::Texture> i_texture;
+  //EDK3::Texture::Load("./test/boat/boat_img.jpg", &b_texture);
+  EDK3::Texture::Load("./test/T_EDK_Logo.png", &i_texture);
+  if(!i_texture){
+    printf("No hay textura de island!!!\n");
+  }
+  
+  // Island Guapa
+  EDK3::ref_ptr<EDK3::Texture> ig_texture;
+  //EDK3::Texture::Load("./test/boat/boat_img.jpg", &b_texture);
+  EDK3::Texture::Load("./test/island_guapa_texture.jpg", &ig_texture);
+  if(!ig_texture){
+    printf("No hay textura de island!!!\n");
   }
 
 
@@ -391,6 +477,18 @@ void InitScene() {
   EDK3::ref_ptr<EDK3::MatDiffuse::Settings> mat_sett_faro;
   mat_sett_faro.alloc();
   mat_sett_faro->set_color(faro_color);
+  
+  // Island
+  float island_color[4] = {0.1f, 1.0f, 0.1f, 1.0f};
+  EDK3::ref_ptr<EDK3::MatDiffuseTexture::Settings> mat_sett_island;
+  mat_sett_island.alloc();
+  //mat_sett_island->set_color(island_color);
+  mat_sett_island->set_texture(i_texture.get());
+  
+  // Island Guapa
+  EDK3::ref_ptr<EDK3::MatDiffuseTexture::Settings> mat_sett_island_guapa;
+  mat_sett_island_guapa.alloc();
+  mat_sett_island_guapa->set_texture(ig_texture.get());
 
   // Cube Map
   EDK3::ref_ptr<EDK3::MaterialCubeMap::Settings> mat_custom_settings;
@@ -408,7 +506,6 @@ void InitScene() {
 
 
   // Lights
-
   dirLight.alloc();
   dirLight->active = 1;
   dirLight->dir[0] = 1.0f;
@@ -428,26 +525,29 @@ void InitScene() {
   pointLight->diffuse_color[0] = 1.0f;
   pointLight->diffuse_color[1] = 0.0f;
   pointLight->diffuse_color[2] = 0.0f;
-  pointLight->pos[0] = boatPos.x;
-  pointLight->pos[1] = boatPos.y +2.0f;
-  pointLight->pos[2] = boatPos.z;
+  //pointLight->pos[0] = boatPos.x;
+  //pointLight->pos[1] = boatPos.y +2.0f;
+  //pointLight->pos[2] = boatPos.z;
+  pointLight->pos[0] = 0.0f;
+  pointLight->pos[1] = 5.27f;
+  pointLight->pos[2] = -150.8;
   pointLight->specular_color[0] = 1.0f;
   pointLight->specular_color[1] = 1.0f;
   pointLight->specular_color[2] = 1.0f;
   pointLight->specular_strength = 0.003f;
   pointLight->specular_shininess = 32.0f;
-  pointLight->constant_att = 2.37f;
-  pointLight->linear_att = -0.52f;
-  pointLight->quadratic_att = 0.045f;
+  pointLight->constant_att = 1.0f;
+  pointLight->linear_att = 0.045f;
+  pointLight->quadratic_att = -0.003f;
 
   spotLight.alloc();
   spotLight->active = 1;
   /*spotLight->pos[0] = faroPos.x;
   spotLight->pos[1] = faroPos.y + 10.0f;
   spotLight->pos[2] = faroPos.z;*/
-  spotLight->pos[0] = -100.0f;
+  spotLight->pos[0] = -21.62f;
   spotLight->pos[1] = 42.03f;
-  spotLight->pos[2] = 3.42f;
+  spotLight->pos[2] = -81.f;
   spotLight->dir[0] = 0.71f;
   spotLight->dir[1] = -0.53f;
   spotLight->dir[2] = -0.59f;
@@ -492,7 +592,7 @@ void InitScene() {
   node->set_material(mat.get());
   node->set_material_settings(mat_settings.get());
   node->set_position(-1.0f, 0.0f, -1.0f);
-  root->addChild(node.get());  
+  root->addChild(node.get());  // 0
 
 
   
@@ -509,7 +609,7 @@ void InitScene() {
     drawable->set_position(boatPos.x, boatPos.y, boatPos.z);
     boat_node->addChild(drawable.get());
   }
-  root->addChild(boat_node.get());
+  root->addChild(boat_node.get()); // 1
   
   // Faro
   EDK3::ref_ptr<EDK3::Node> faro_node;
@@ -525,10 +625,9 @@ void InitScene() {
     drawable->set_position(0.0f, 0.0f, 0.0f);
     faro_node->addChild(drawable.get());
   }
-  root->addChild(faro_node.get());
+  root->addChild(faro_node.get()); // 2
 
   // Sphere
-
   EDK3::ref_ptr<EDK3::Drawable> sphere_drawable;
   EDK3::ref_ptr<EDK3::Node> sphere_node;
   sphere_pos[0] = 9.0f;
@@ -539,15 +638,15 @@ void InitScene() {
   sphere_drawable->set_position(sphere_pos[0], sphere_pos[1], sphere_pos[2]);
 
   // Sphere pivot
-  sphere_pivot[0] = -63.0f;
-  sphere_pivot[1] = 43.230f;
-  sphere_pivot[2] = 15.53f;
+  sphere_pivot[0] = -21.62f;
+  sphere_pivot[1] = 42.0f;
+  sphere_pivot[2] = -81.0f;
 
   sphere_node.alloc();
   sphere_node->set_position(sphere_pivot[0], sphere_pivot[1], sphere_pivot[2]);
   sphere_node->addChild(sphere_drawable.get());
     
-  root->addChild(sphere_node.get());
+  root->addChild(sphere_node.get()); // 3
   
 
   // Cube map
@@ -559,15 +658,52 @@ void InitScene() {
   cube->set_material(mat_custom.get());
   cube->set_material_settings(mat_custom_settings.get());
   cube->set_HPR(0.0f, 0.0f, 0.0f);
-  root->addChild(cube.get());
+  root->addChild(cube.get()); // 4
 
-  // Surface
-  node.alloc();
-  node->set_geometry(surface.get());
-  node->set_material(mat_surface.get());
-  node->set_material_settings(surface_mat_settings.get());
-  node->set_position(surface_position[0], surface_position[1], surface_position[2]);
-  root->addChild(node.get());
+  // Island
+  EDK3::ref_ptr<EDK3::Node> island_node;
+
+  island_node.alloc();
+  island_node->set_position(island_pos[0], island_pos[1], island_pos[2]);
+  //island_node->set_rotation_y(island_rotation * (j * (3.1415f / 3.0f)));
+  for(int i = 0; i < island.size(); i++){
+    drawable.alloc();
+    drawable->set_geometry(island[i].get());
+    drawable->set_material(mat_island.get());
+    drawable->set_material_settings(mat_sett_island.get());
+    drawable->set_position(0.0f, 0.0f, 0.0f);
+    island_node->addChild(drawable.get());
+  }
+  root->addChild(island_node.get()); // 5
+  
+  island_node.alloc();
+  island_node->set_position(-143.0f, 149.8f, -196.7f);
+  island_node->set_rotation_y(108.6f);
+  island_node->set_scale(1.2f, 1.2f, 1.2f);
+  for(int i = 0; i < island.size(); i++){
+    drawable.alloc();
+    drawable->set_geometry(island[i].get());
+    drawable->set_material(mat_island.get());
+    drawable->set_material_settings(mat_sett_island.get());
+    drawable->set_position(0.0f, 0.0f, 0.0f);
+    island_node->addChild(drawable.get());
+  }
+  root->addChild(island_node.get()); // 6
+
+  // Island Guapa
+  island_node.alloc();
+  island_node->set_position(0.0f, 0.0f, 0.0f);
+  //island_node->set_rotation_y(island_rotation * (j * (3.1415f / 3.0f)));
+  for(int i = 0; i < island_guapa.size(); i++){
+    drawable.alloc();
+    drawable->set_geometry(island_guapa[i].get());
+    drawable->set_material(mat_island_guapa.get());
+    drawable->set_material_settings(mat_sett_island_guapa.get());
+    drawable->set_position(0.0f, 0.0f, 0.0f);
+    island_node->addChild(drawable.get());
+  }
+  root->addChild(island_node.get()); // 7
+  
 
   // Water
   node.alloc();
@@ -575,7 +711,16 @@ void InitScene() {
   node->set_material(mat_water.get());
   node->set_material_settings(water_mat_settings.get());
   node->set_position(-1.0f,0.0f,-1.0f);
-  root->addChild(node.get()); 
+  root->addChild(node.get()); // 8
+
+
+  // Surface
+  node.alloc();
+  node->set_geometry(surface.get());
+  node->set_material(mat_surface.get());
+  node->set_material_settings(surface_mat_settings.get());
+  node->set_position(surface_position[0], surface_position[1], surface_position[2]);
+  root->addChild(node.get()); // 9
 
 
   // Post Process
@@ -711,7 +856,8 @@ void UpdateFn(double dt) {
   sphere_center->set_position(sphere_pivot[0],sphere_pivot[1],sphere_pivot[2]);
   sphere_center->set_rotation_y(ESAT::Time() * 0.005f * sphere_rotation_speed);
 
-  SetLightToBolinga();
+  //SetLightToBolinga();
+  sphere_center->child(0)->set_position(sphere_pos[0],sphere_pos[1],sphere_pos[2]);
 
 
   EDK3::ref_ptr<EDK3::Node> boat = GameState.root->child(1);
@@ -725,16 +871,34 @@ void UpdateFn(double dt) {
   EDK3::ref_ptr<EDK3::Node> faro = GameState.root->child(2);
   faro->set_position(faroPos.x, faroPos.y, faroPos.z);
   
-  EDK3::ref_ptr<EDK3::Node> piedra1 = faro->child(6);
 
   for(int i = 0; i < GameState.root->num_children(); i++){
-    EDK3::ref_ptr<EDK3::Node> child = GameState.root->child(i);
+    //EDK3::ref_ptr<EDK3::Node> child = GameState.root->child(i);
   }
 
-  EDK3::ref_ptr<EDK3::Node> surface = GameState.root->child(5);
+  EDK3::ref_ptr<EDK3::Node> surface = GameState.root->child(9);
   surface->set_position(surface_position[0], surface_position[1], surface_position[2]);
-  surface->set_rotation_y(ESAT::Time() * 0.005f * surface_rotation_speed);
-  //surface->set_scale(50.0f,50.0f,50.0f);
+  //surface->set_rotation_y(ESAT::Time() * 0.005f * surface_rotation_speed);
+  //surface->set_rotation_x(ESAT::Time() * 0.005f * surface_rotation_speed);
+  //surface->set_rotation_z(ESAT::Time() * 0.005f * surface_rotation_speed);
+  surface->set_scale(surface_scale[0], surface_scale[1], surface_scale[2]);
+
+  //for(int i = 6; i < 6+3; i++){
+
+    EDK3::ref_ptr<EDK3::Node> island = GameState.root->child(5);
+    island->set_position(island_pos[0], island_pos[1], island_pos[2]);
+    island->set_rotation_y(island_rotation);
+    island->set_scale(island_scale,island_scale,island_scale);
+  //}
+  
+  EDK3::ref_ptr<EDK3::Node> isla_guapa = GameState.root->child(7);
+  isla_guapa->set_position(isla_guapa_pos[0], isla_guapa_pos[1], isla_guapa_pos[2]);
+  isla_guapa->set_rotation_xyz(isla_guapa_rotation[0], isla_guapa_rotation[1], isla_guapa_rotation[2]);
+
+
+  EDK3::ref_ptr<EDK3::Node> terrain = GameState.root->child(0);
+  terrain->set_position(terrain_pos[0],terrain_pos[1],terrain_pos[2]);
+  terrain->set_scale(terrain_scale[0],terrain_scale[1],terrain_scale[2]);
 
   //piedra1->set_position(piedra_pos[0], piedra_pos[1], piedra_pos[2]);
   //faro->set_rotation_y(ESAT::Time() * 0.001f * piedra_rotation);
@@ -793,11 +957,16 @@ void ImGuiFn(double dt) {
   ImGui::Checkbox("Follow Boat",&followBoat_);
   GameState.camera->following_ = followBoat_;
 
+  if(ImGui::CollapsingHeader("Terrain")){
+    ImGui::DragFloat3("Position",terrain_pos,0.1f,-300.0f,300.0f);
+    ImGui::DragFloat3("Scale",terrain_scale,0.1f,0.0f,100.0f);
+  }
+
   if(ImGui::CollapsingHeader("Boat")){
     ImGui::Text("Position");
-    ImGui::DragFloat("Boat X: ",&boatPos.x, 0.001f, -1000.0f,1000.0f, "%f");
-    ImGui::DragFloat("Boat Y: ",&boatPos.y, 0.001f, -1000.0f,1000.0f, "%f");
-    ImGui::DragFloat("Boat Z: ",&boatPos.z, 0.001f, -1000.0f,1000.0f, "%f");
+    ImGui::DragFloat("Boat X: ",&boatPos.x, 0.1f, -1000.0f,1000.0f, "%f");
+    ImGui::DragFloat("Boat Y: ",&boatPos.y, 0.1f, -1000.0f,1000.0f, "%f");
+    ImGui::DragFloat("Boat Z: ",&boatPos.z, 0.1f, -1000.0f,1000.0f, "%f");
     
   }
   
@@ -808,7 +977,7 @@ void ImGuiFn(double dt) {
     ImGui::DragFloat("Faro Z: ",&faroPos.z, 0.1f, -1000.0f,1000.0f, "%f");
     
     if(ImGui::CollapsingHeader("Bolinga")){
-      ImGui::DragFloat3("Pivot",sphere_pivot,0.1f,-100.0f,100.0f);
+      ImGui::DragFloat3("Pivot",sphere_pivot,0.1f,-200.0f,200.0f);
       ImGui::DragFloat3("Sphere",sphere_pos,0.1f,-100.0f,100.0f);
       ImGui::DragFloat("Rotation Speed",&sphere_rotation_speed,0.01f, -50.0f, 50.0f);
     }
@@ -824,9 +993,9 @@ void ImGuiFn(double dt) {
 
     if(ImGui::CollapsingHeader("Point")){
       ImGui::Text("Point Boat");
-      ImGui::DragFloat("Position X: ",&pointLight->pos[0], 0.01f, -100.0f,100.0f, "%f");
-      ImGui::DragFloat("Position Y: ",&pointLight->pos[1], 0.01f, -100.0f,100.0f, "%f");
-      ImGui::DragFloat("Position Z: ",&pointLight->pos[2], 0.01f, -100.0f,100.0f, "%f");
+      ImGui::DragFloat("Position X: ",&pointLight->pos[0], 0.1f, -200.0f,200.0f, "%f");
+      ImGui::DragFloat("Position Y: ",&pointLight->pos[1], 0.1f, -200.0f,200.0f, "%f");
+      ImGui::DragFloat("Position Z: ",&pointLight->pos[2], 0.1f, -200.0f,200.0f, "%f");
 
       ImGui::DragFloat("Constant ",&pointLight->constant_att, 0.005f, -100.0f,100.0f, "%f");
       ImGui::DragFloat("Linear ",&pointLight->linear_att, 0.005f, -100.0f,100.0f, "%f");
@@ -1034,8 +1203,19 @@ void ImGuiFn(double dt) {
   }
 
   if(ImGui::CollapsingHeader("Surface revolution")){
-    ImGui::DragFloat3("Position",surface_position, 0.01f, -100.0f, 100.0f);
+    ImGui::DragFloat3("Position",surface_position, 0.01f, -400.0f, 400.0f);
     ImGui::DragFloat("Rotation",&surface_rotation_speed, 0.01f, -100.0f, 100.0f);
+    ImGui::DragFloat3("Sacle", surface_scale,0.1f, 0.0f, 100.0f);
+  }
+
+  if(ImGui::CollapsingHeader("Island")){
+    ImGui::DragFloat3("Position", island_pos, 0.1f, -600.0f, 600.0f);
+    ImGui::DragFloat("Rotation Y", &island_rotation, 0.1f, -360.0f, 360.0f);
+    ImGui::DragFloat("Scale", &island_scale, 0.1f, -360.0f, 360.0f);
+    ImGui::Text("Isla guapa");
+    ImGui::DragFloat3("Position G", isla_guapa_pos, 0.1f, -600.0f, 600.0f);
+    ImGui::DragFloat3("Rotation G", isla_guapa_rotation, 0.1f, -200.0f, 200.0f);
+    
   }
 
   ImGui::Checkbox("WireFrame",&wireFrame2_);
